b641ea4d8fcfc0ff5ef304af4d9ed9c725df1089
JuicyBram <bvdwallen1912@gmail.com>
2026-03-12 12:07:07 +0100

    Added changes to the sum-service

--- main-application/sum-service/sum.test.ts
+++ main-application/sum-service/sum.test.ts
@@ -1,4 +1,4 @@
-import { sum, sumArray } from './sum';
+import { sum, sumArray, hardCalculation } from './sum';
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
@@ -6,4 +6,8 @@ test('adds 1 + 2 to equal 3', () => {
 
 test('sums an array of numbers', () => {
   expect(sumArray([1, 2, 3])).toBe(6);
+});
+
+test('performs a hard calculation', () => {
+  expect(hardCalculation(1, 2, 3)).toBe(7); // 1 + 2 * 3 = 7
 });
