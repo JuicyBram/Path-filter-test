95edd02526534c01bd29036cac2ef18565b19af2
JuicyBram <bvdwallen1912@gmail.com>
2026-03-20 12:26:59 +0100

    Added test

--- main-application/sum-service/sum.test.ts
+++ main-application/sum-service/sum.test.ts
@@ -4,9 +4,14 @@ test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
 
-test('sums an array of numbers', () => {
+test('sums an array of 3 numbers', () => {
   expect(sumArray([1, 2, 3])).toBe(6);
 });
+
+test('sums an array of 4 numbers', () => {
+  expect(sumArray([1, 2, 3, 4])).toBe(10);
+});
+
 
 test('performs a hard calculation', () => {
   expect(hardCalculation(1, 2, 3)).toBe(7); // 1 + 2 * 3 = 7
